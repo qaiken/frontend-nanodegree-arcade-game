f8c11b7252f5955533c56cf9010bbb07005f7e63
Quinton Aiken <quintonlaiken@gmail.com>
2015-03-13 16:53:52 -0500

    add enemies

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.speed = Math.random() * 75 + 50;
+    this.x = x;
+    this.y = y;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,23 +17,52 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
+    this.x += this.speed * dt;
+
+    if(this.x > Enemy.canvas.width + 50)
+      this.x = -100;
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+};
+
+Player.prototype.update = function() {
+};
+
+Player.prototype.render = function() {
+
+};
+
+Player.prototype.handleInput = function(dir) {
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [],
+    player = new Player();
 
+Enemy.addEnemies = function() {
+  var i = 0;
+  while(i < 4) {
+    allEnemies.push(new Enemy(Math.random() * Enemy.canvas.width, (i * 83) + 55 ));
+    ++i;
+  }
+};
+
+Enemy.canvas = document.querySelector('canvas');
+
+Enemy.addEnemies();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
